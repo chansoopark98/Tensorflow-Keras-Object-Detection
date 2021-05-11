ef7d791573272ce0d9accc7142102ba634445757
chansoopark98 <60956651+chansoopark98@users.noreply.github.com>
2021-05-11 23:38:02 +0900

    focal loss normalize

--- model/focal_loss.py
+++ model/focal_loss.py
@@ -200,7 +200,7 @@ def total_loss(y_true, y_pred, num_classes=21):
     num_pos = tf.cast(tf.shape(gt_locations)[0], tf.float32)
     # divide num_pos objects
     loc_loss = smooth_l1_loss / num_pos
-
+    focal_loss = focal_loss / num_pos
     mbox_loss = loc_loss + focal_loss
     return mbox_loss
 
