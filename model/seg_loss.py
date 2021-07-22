33d4cfc9ce5d14150e2ab1d2ae3843b8158888d8
chansoopark98 <60956651+chansoopark98@users.noreply.github.com>
2021-07-22 21:39:22 +0900

    Update seg_loss.py

--- model/seg_loss.py
+++ model/seg_loss.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
-
+import sys
 class Seg_loss:
     def __init__(self, batch_size):
         self.batch_size = batch_size
 
     def total_loss(self, y_true, y_pred):
         y_pred = tf.nn.log_softmax(y_pred, axis=-1)
-        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True,
-                                                             reduction=tf.keras.losses.Reduction.SUM)(y_true, y_pred)
-
-        loss = loss * (1. / self.batch_size)
+        loss = tf.math.reduce_mean(tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)(y_true, y_pred))
+        tf.print(loss, sys.stdout)
+        #loss = loss * (1. / self.batch_size)
         return loss
