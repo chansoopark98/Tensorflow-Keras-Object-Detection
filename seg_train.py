61a95b6308b3a6f864e963166bfeef1b571b1ae4
ChanSoo Park <chansoopark@kw.ac.kr>
2021-07-23 22:23:07 +0900

    update

--- seg_train.py
+++ seg_train.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size",     type=int,   help="배치 사이즈값 설정", default=16)
 parser.add_argument("--epoch",          type=int,   help="에폭 설정", default=100)
 parser.add_argument("--lr",             type=float, help="Learning rate 설정", default=0.005)
-parser.add_argument("--weight_decay",   type=float, help="Weight Decay 설정", default=0.0005)
+parser.add_argument("--weight_decay",   type=float, help="Weight Decay 설정", default=0.0001)
 parser.add_argument("--model_name",     type=str,   help="저장될 모델 이름",
                     default=str(time.strftime('%m%d', time.localtime(time.time()))))
 parser.add_argument("--dataset_dir",    type=str,   help="데이터셋 다운로드 디렉토리 설정", default='./datasets/')
