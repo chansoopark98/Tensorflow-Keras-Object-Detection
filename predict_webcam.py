7932c7828f7398c01a71b38a1f56558704e95e52
park-tsp <chansoo0710@gmail.com>
2022-08-11 19:05:11 +0900

    Add: predict on webcam.py

--- predict_webcam.py
+++ predict_webcam.py
@@ -8,12 +8,22 @@ from model.model_builder import ModelBuilder
 from utils.misc import draw_bounding, CLASSES, COCO_CLASSES, TEST_CLASSES
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--backbone_name",       type=str,   help="Pretrained backbone name",
+parser.add_argument("--backbone_name",    type=str,    help="Pretrained backbone name\
+                                                            |   model_name    : description | \
+                                                            [ 1. mobilenetv2       : MobileNetV2 ]\
+                                                            [ 2. mobilenetv3s      : MobileNetV3-Small ] \
+                                                            [ 3. mobilenetv3l      : MobileNetV3-Large ] \
+                                                            [ 4. efficient_lite_v0 : EfficientNet-Lite-B0 ]\
+                                                            [ 5. efficientnetv2b0  : EfficientNet-V2-B0 ]\
+                                                            [ 6. efficientnetv2b3  : EfficientNet-V2-B3 ]",
                     default='efficient_lite_v0')
 parser.add_argument("--num_classes",          type=int,   help="Number of classes in the pretrained model",
                     default=2)
-parser.add_argument("--train_dataset_type",  type=str,   help="Train dataset type",
-                    default='voc')
+parser.add_argument("--image_norm_type",  type=str,    help="Set RGB image nornalize format (tf or torch or no)\
+                                                             [ 1. tf    : Rescaling RGB image -1 ~ 1 from imageNet ]\
+                                                             [ 2. torch : Rescaling RGB image 0 ~ 1 from imageNet ]\
+                                                             [ 3. else  : Rescaling RGB image 0 ~ 1 only divide 255 ]",
+                    default='div')
 parser.add_argument("--image_size",          type=tuple, help="Model image size (input resolution)",
                     default=(300, 300))
 parser.add_argument("--threshold",           type=float, help="Post processing confidence threshold",
@@ -27,12 +37,21 @@ parser.add_argument("--gpu_num",             type=int,    help="Set GPU number t
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    # Set num classes and label list by args.train_dataset_type
     tf.config.set_soft_device_placement(True)
 
     gpu_number = '/device:GPU:' + str(args.gpu_num)
     with tf.device(gpu_number):
-        label_list = TEST_CLASSES
+        if args.num_classes == 21:
+            # PASCAL VOC
+            label_list = CLASSES
+
+        elif args.num_classes == 81:
+            # COCO2017
+            label_list = COCO_CLASSES
+        else:
+            # Custom dataset ('0', '1', '2', '3' ...)
+            custom_label_list = range(args.num_classes)
+            label_list = [str(label_iter) for label_iter in custom_label_list]
 
         # Set target transforms
         spec_list = convert_spec_list()
@@ -58,7 +77,13 @@ if __name__ == '__main__':
             img = tf.image.resize(img, size=args.image_size,
                     method=tf.image.ResizeMethod.BILINEAR)
             img = tf.cast(img, tf.float32)
-            img /= 255.
+
+            if args.image_norm_type == 'torch':
+                img = preprocess_input(img, mode='torch')
+            elif args.image_norm_type == 'tf':
+                img = preprocess_input(img, mode='tf')
+            else:
+                img /= 255
             
             img = tf.expand_dims(img, axis=0)
 
@@ -71,7 +96,6 @@ if __name__ == '__main__':
             if pred_boxes.size > 0:
                 draw_bounding(frame, pred_boxes,  labels=pred_labels,  scores=pred_scores, img_size=frame.shape[:2], label_list=label_list)
             
-
             cv2.imshow("VideoFrame", frame)
 
         capture.release()
